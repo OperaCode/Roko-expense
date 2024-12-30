cacd9a45eadc70cc9dc9351775fd77d24fed3bdd
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-12-30 23:56:14 +0100

    login2

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,32 +1,60 @@
-import React from 'react'
-import background from "../assets/login-bg.jpg"
+import React from "react";
+// import background from "../assets/login-bg.jpg"
 
 const Login = () => {
   return (
-    <main className='bg-[url(../assets/login-bg.jpg)]' >
+    <main className="bg-indigo-500 ">
+      <div className="p-20 justify-between">
+        <div>
+          <p className="my-6 text-white">
+            <span></span>Back to home page
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <div className=" space-y-5 text-white px-6">
+            <h1 className="font-bold text-5xl ">Welcome Back!</h1>
+            <h3 className="text-md">Login to make seamless transaction records effortlessly</h3>
 
-      <div className='flex justify-between'>
-        <p><span></span>Back to home page</p>
+            <p>New to Roko ?</p>
+            <button className="bg-slate-200 text-black p-2 w-2/3">Sign up</button>
+          </div>
 
-        <h1 className='font-bold text-2xl'>Welcome Back!</h1>
-        <h3>Login to make seamless transaction records effortlessly</h3>
-        
-        <p>New to Roko ?</p>
-        <button>Sign up</button>
+          <div className=" bg-slate-100 p-6">
+            <div className="text-center">
+              <h1 className="text-2xl font-bold">Login</h1>
+              <p>
+                Please fill in the following details to log into your account
+              </p>
+            </div>
 
+            <form action="" className=" ">
+              <div className=" md:flex-col">
+                <label htmlFor="email" className="font-semibold">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  placeholder="Please enter email address"
+                  required
+                  className="p-1"
+                />
+                <label htmlFor="password" className="font-semibold">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  className="p-1"
+                  required
+                />
+              </div>
+              <button className="bg-indigo-600 text-white p-3 w-full">Login</button>
+            </form>
+          </div>
+        </div>
       </div>
-
-      <div >
-        <form action="" className='flex flex-col'>
-          <label htmlFor="email">Email Address</label>
-          <input type="email" required />
-          <label htmlFor="password">Password</label>
-          <input type="password" required/>
-        </form>
-      </div>
-    
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
