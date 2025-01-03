deedd749b163f580e90782a6b14c6ba85bb8cc42
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2025-01-03 23:19:19 +0100

    login-bg

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,54 +1,75 @@
 import React from "react";
+import { GoArrowLeft } from "react-icons/go";
+import { FaRegEye } from "react-icons/fa6";
 // import background from "../assets/login-bg.jpg"
+import "./Login.css";
 
 const Login = () => {
   return (
-    <main className="bg-indigo-500 ">
-      <div className="p-20 justify-between">
-        <div>
-          <p className="my-6 text-white">
-            <span></span>Back to home page
-          </p>
+    <main className="bg-indigo-500 background p-10">
+      <div className="p-6">
+        <div className="flex items-center gap-1 mb-4 text-white">
+          <GoArrowLeft />
+          <p>Back to home Page</p>
         </div>
-        <div className="flex justify-between">
-          <div className=" space-y-5 text-white px-6">
-            <h1 className="font-bold text-5xl ">Welcome Back!</h1>
-            <h3 className="text-md">Login to make seamless transaction records effortlessly</h3>
 
-            <p>New to Roko ?</p>
-            <button className="bg-slate-200 text-black p-2 w-2/3">Sign up</button>
-          </div>
-
-          <div className=" bg-slate-100 p-6">
-            <div className="text-center">
-              <h1 className="text-2xl font-bold">Login</h1>
-              <p>
-                Please fill in the following details to log into your account
+        <div className=" md:flex justify-between">
+          {/* HeroCard */}
+          <div className=" text-white space-y-10 py-20">
+            <div className="space-y-4">
+              <h1 className="text-5xl font-bold font-serif">Welcome Back!</h1>
+              <p className="text-lg">
+                Login to make sealess transaction records effortlessly.
               </p>
             </div>
 
-            <form action="" className=" ">
-              <div className=" md:flex-col">
+            <div>
+              <p>New to RokoApp?</p>
+              <p className="">Sign Up</p>
+            </div>
+          </div>
+
+          {/* form Card */}
+          <div className=" bg-slate-200 p-10">
+            <div className="text-center ">
+              <h1 className="text-4xl">Login</h1><br />
+              <p>
+                Please fill in the following details to log into your acount
+              </p>
+            </div>
+            <br />
+
+            <form action="" className="  gap-3">
+              <div className="gap-4">
                 <label htmlFor="email" className="font-semibold">
-                  Email Address
+                  Email
                 </label>
                 <input
                   type="email"
-                  placeholder="Please enter email address"
+                  id="email"
+                  placeholder="Pleasefill your email"
                   required
-                  className="p-1"
                 />
+              </div>
+              <br />
+              <div className="flex gap-4">
                 <label htmlFor="password" className="font-semibold">
                   Password
                 </label>
                 <input
                   type="password"
-                  placeholder="Enter Password"
-                  className="p-1"
+                  id="password"
+                  placeholder="Enter password"
                   required
                 />
+                <FaRegEye className="-ml-10" />
               </div>
-              <button className="bg-indigo-600 text-white p-3 w-full">Login</button>
+
+              <div className="flex justify-center pt-24">
+                <button className="bg-indigo-500 p-3 w-2/3 j">
+                  Login
+                </button>
+              </div>
             </form>
           </div>
         </div>
