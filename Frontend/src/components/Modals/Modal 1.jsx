2391f100343b9ff086fee41af13d704fa74ba838
opechapo <abolajiopeyemi000@gmail.com>
2025-01-10 12:03:13 +0100

    checking pull..

---
+++ Frontend/src/components/Modals/Modal 1.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const Modal_1 = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-blue-300 rounded-lg shadow-lg p-10 w-60 relative">
+
+            <button
+              onClick={closeModal}
+              className="absolute top-2 left-2 text-gray-500 hover:text-gray-700 text-3xl font-bold"
+            >
+              &times;
+            </button>
+
+            <br />
+
+            <div className="flex flex-col gap-4">
+              <button className="px-4 py-2 bg-indigo-900 text-white rounded hover:bg-indigo-800">
+                PDF
+              </button>
+              <button className="px-4 py-2 bg-indigo-900 text-white rounded hover:bg-indigo-800">
+                Image
+              </button>
+              <button className="px-4 py-2 bg-indigo-900 text-white rounded hover:bg-indigo-800">
+                Excel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modal_1;
