da9353ba395618c895ebef97ca88397d6b7289f8
Raphael faboyinde <raphaelfaboyinde27@gmail.com>
2025-01-13 10:07:04 +0100

    Revert "Staging"

--- Frontend/src/components/Dashboard/HomeDash.jsx
+++ Frontend/src/components/Dashboard/HomeDash.jsx
@@ -1,29 +1,12 @@
-import React from "react";
-import Graph from "../Layouts/Graph";
-
+import React from 'react'
+import "./HomeDash.css"
 
 const HomeDash = () => {
   return (
-    <section className="md:flex justify-between gap-5 items-center  w-full ">
-      {/* Total Incomes and Funding */}
+    <div>
 
-      <div className=" flex-1 space-y-3 flex-col items-center">
-        <div className="p-2 bg-white rounded-lg shadow-md text-center ">
-          <p className="text-gray-600 text-sm md:text-lg">Total Incomes and Funding</p>
-          <h2 className="md:text-2xl font-bold text-gray-800">$9,450.00</h2>
-        </div>
+    </div>
+  )
+}
 
-        <div className="p-2 bg-white rounded-lg shadow-md text-center ">
-          <p className="text-gray-600 text-sm">Total Expenses</p>
-          <h2 className="md:text-2xl font-bold text-gray-800">$3,945.55</h2>
-        </div>
-      </div>
-      {/* Expenses by Category Chart */}
-      <div className="flex-1 py-4  ">
-        <Graph  />
-      </div>
-    </section>
-  );
-};
-
-export default HomeDash;
+export default HomeDash
--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,69 +1,71 @@
 import React from "react";
 import { GoArrowLeft } from "react-icons/go";
+import { FaRegEye } from "react-icons/fa6";
+
 import "./Login.css";
-import PasswordInput from "./PasswordInput";
-import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <main className="bg-indigo-500 background h-screen p-16 font-bodyFont">
+    <main className="bg-indigo-500 background p-16 ">
       <div className="">
-        <Link to='/'><div className="flex items-center gap-1 text-white hover:cursor-pointer">
+        <div className="flex items-center gap-1 text-white hover:cursor-pointer">
           <GoArrowLeft />
           <p>Back to home Page</p>
-        </div></Link>
+        </div>
 
         <div className=" md:flex ">
           {/* HeroCard */}
           <div className=" text-white space-y-10 py-20 md:flex-1">
-            <div className="space-y-4 pr-12 font-headerFont flex flex-col gap-2">
-              <h1 className="text-5xl font-bold">Welcome Back!</h1>
-              <p className="text-2xl lg:w-8/12">
+            <div className="space-y-4 pr-12">
+              <h1 className="text-5xl font-bold font-serif">Welcome Back!</h1>
+              <p className="text-3xl">
                 Login to make seamless transaction records effortlessly.
               </p>
             </div>
 
             <div className="">
-              <p className="mb-4">New to RokoApp?</p>
-             <Link to='/register'><p className="bg-indigo-200 text-black rounded w-1/2 p-3 text-center hover:cursor-pointer font-semibold">Sign Up</p></Link>
+              <p className="mb-8">New to RokoApp?</p>
+              <p className="bg-indigo-200 text-black rounded w-1/2 p-3 text-center hover:cursor-pointer font-semibold">Sign Up</p>
             </div>
           </div>
 
           {/* form Card */}
-          <div className=" bg-white p-6 rounded md:flex-1">
+          <div className=" bg-slate-200 p-6 rounded md:flex-1">
             <div className="text-center ">
-              <h1 className="text-4xl font-bold font-headerFont">Login</h1><br />
+              <h1 className="text-4xl font-bold ">Login</h1><br />
               <p>
                 Please fill in the following details to log into your acount
               </p>
             </div><br />
            
 
-            <form action="" className="px-16 flex flex-col gap-5">
-            <div className="flex flex-col gap-1">
-                    <label htmlFor="email" className="font-medium">
-                   Email
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      placeholder="Please fill your email"
-                      className="bg-indigo-100 p-2 rounded-md border-b border-indigo-900"
-                      required
-                    />
-                  </div>
+            <form action="" className="px-16">
+              <div className="flex flex-col gap-4">
+                <label htmlFor="email" className="font-semibold">
+                  Email 
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Please fill your email"
+                  className="bg-indigo-100 p-3 border border-black"
+                  required
+                />
+              </div>
              
-              <div className="flex flex-col gap-1">
-                <label htmlFor="password" className="font-medium">
+              <div className="flex flex-col gap-4">
+                <label htmlFor="password" className="font-semibold">
                   Password
                 </label>
                 <div className="">
-                <PasswordInput
+                <input
                   type="password"
                   id="password"
                   placeholder="Enter password"
+                   className="bg-indigo-100 p-3 border border-black w-full"
                   required
                 />
+                <FaRegEye className="-mt-8 right-48 absolute flex justify-end  cursor-pointer " />
                 </div>
               </div>
 
